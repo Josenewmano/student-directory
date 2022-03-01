56a28810c2658af3a71064d4d7ca386a539c2deb
Joe <j.newman@live.co.uk>
2022-03-01 21:41:10 +0000

    Second attemot at ga refectored menu :)

--- directory.rb
+++ directory.rb
@@ -1,9 +1,8 @@
+@students = []
+
 def input_students
   puts "Please enter the details of the students"
-  puts "To finish, hit return four times"
-
-  students = []
-  
+  puts "To finish, hit return four times"  
   puts "Name"
   name = gets.chomp
   puts "Cohort"
@@ -17,12 +16,12 @@ def input_students
 
   while !name.empty? && !country.empty? && !height.empty? do
     
-    students << {name: name, 
+    @students << {name: name, 
                 cohort: cohort, 
                 country: country, 
                 height: height}
 
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
 
     puts "Name"
     name = gets.chomp
@@ -35,8 +34,7 @@ def input_students
     height = gets.chomp
     
   end
-
-  students
+  @students
 end
 
 def print_header
@@ -44,63 +42,66 @@ def print_header
   puts ("-------------").center(50, "------------------")
 end
 
-def print_march(students)
-  march_students = students.select {|student| student[:cohort] == "March" }
-  march_students.each.with_index(1) do |student, index|
+def print(students)
+  @students.each.with_index(1) do |student, index|
     puts ("#{index}. #{student[:name]} (#{student[:cohort]} cohort)").center(50)
   end
 end
 
 def print_footer(students)
-  march_students = students.select {|student| student[:cohort] == "March" }
-  if march_students.length == 1
-    puts ("Overall, we have #{march_students.count} great student").center(50)
+  if @students.count == 1
+    puts ("Overall, we have #{@students.count} great student").center(50)
   else
-    puts ("Overall, we have #{march_students.count} great students").center(50)
+    puts ("Overall, we have #{@students.count} great students").center(50)
   end
 end
 
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    
-    selection = gets.chomp
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-    case selection
+def show_students
+  print_header
+  print(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      @students = input_students
     when "2"
-      print_header
-      print_march(students)
-      print_footer(students)
+      show_students
     when "3"
-      
+  
     when "4"
-      
+  
     when "5"
-     
+ 
     when "6"
-    
+
     when "7"
-     
+ 
     when "8"
-    
+
     when "9"
       exit 
     else
       puts "I'm not sure what you meant, try again"
       puts "Type a number"
-    end
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu 
+    process(gets.chomp)
   end
 end
 
 interactive_menu
 
-loop do
-  if students.length == 0
-    break
-  end
-end
+#If selecting for March later
+#march_students = @students.select {|student| student[:cohort] == "March" }
