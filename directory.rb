fc7d1f31f19797af225d7c316d8889e4cb286d08
Joe <j.newman@live.co.uk>
2022-03-01 21:03:41 +0000

    Cleaned up & interactive directory

--- directory.rb
+++ directory.rb
@@ -1,36 +1,69 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the details of the students"
+  puts "To finish, hit return four times"
 
   students = []
-
+  
+  puts "Name"
   name = gets.chomp
+  puts "Cohort"
+  cohort = gets.chomp
+  cohort = "March" if cohort.empty?
+  puts "Country of birth"
+  country = gets.chomp
+  puts "Height in centimetres"
+  height = gets.chomp
 
-  while !name.empty? do
-    students << {name: name, cohort: :March}
+
+  while !name.empty? && !country.empty? && !height.empty? do
+    
+    students << {name: name, 
+                cohort: cohort, 
+                country: country, 
+                height: height}
+
     puts "Now we have #{students.count} students"
+
+    puts "Name"
     name = gets.chomp
+    puts "Cohort"
+    cohort = gets.chomp
+    cohort = "March" if cohort.empty?
+    puts "Country of birth"
+    country = gets.chomp
+    puts "Height in centimetres"
+    height = gets.chomp
+    
   end
 
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts ("The students of Villains Academy").center(50)
+  puts ("-------------").center(50, "------------------")
 end
 
-def print(students)
-  students.each do |student |
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_march(students)
+  march_students = students.select {|student| student[:cohort] == "March" }
+  march_students.each.with_index(1) do |student, index|
+    puts ("#{index}. #{student[:name]} (#{student[:cohort]} cohort)").center(50)
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  march_students = students.select {|student| student[:cohort] == "March" }
+  if march_students.length == 1
+    puts ("Overall, we have #{march_students.count} great student").center(50)
+  else
+    puts ("Overall, we have #{march_students.count} great students").center(50)
+  end
 end
 
 students = input_students
+if students.length == 0
+  break
+end
 print_header
-print(students)
+print_march(students)
 print_footer(students)
